072caad8bde58d7c95a40807a7e5daa040ed19f7
ahmaddii <malikahmad6666666@gmail.com>
2024-09-09 16:39:18 +0500

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,17 @@
+void main() {
+  String name = 'Hello it is malik ahmad';
+
+  print(name[0]);
+
+  print(name.length);
+  print(name.contains('Hello'));
+
+  print(name.toLowerCase());
+  print(name.toUpperCase());
+
+  print(name.split(''));
+
+  print(name.replaceAll('Hello', 'Hi'));
+
+  print(name.endsWith('Hleoo'));
+}
